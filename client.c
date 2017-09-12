a0e00d29eedba45d9a9100baf66f8520da22aae3
callum <callum.ide12@gmail.com>
2017-09-12 16:33:16 +1000

    PROGRESS WITH passwords

--- client.c
+++ client.c
@@ -12,7 +12,22 @@ void ErrorMessage(const char *msg){
     exit(1);
 }
  
+ 
+ 
+ void create_game(){
+	
+	printf("======================================\n\n");
+	
+	printf("WELCOME TO THE ONLINE HANGMAN GAMING SYSTEM\n\n");
+	
+	printf("======================================\n\n");
+	
+	
+	
+	
+}
 int main(int argc,char *argv[]){
+	create_game();
     int sock_fd=0, port=0,n=0;
     char buffer[256];
     struct sockaddr_in serv_addr;
@@ -42,11 +57,16 @@ int main(int argc,char *argv[]){
     if (connect(sock_fd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
         ErrorMessage("Error in connection");
  
-    printf("Insert message: \n");
+    printf("PLEASE ENTER YOUR USERNAME-- -> \n");
     bzero(buffer,256);
     fgets(buffer,256,stdin);
     n=write(sock_fd,buffer,strlen(buffer));
  
+	printf("PLEASE ENTER YOUR PASSWORD-- ->  \n");
+    bzero(buffer,256);
+    fgets(buffer,256,stdin);
+    n=write(sock_fd,buffer,strlen(buffer));
+	
     if(n<0)
         ErrorMessage("Error writing in socket");
     bzero(buffer,256);
@@ -55,5 +75,8 @@ int main(int argc,char *argv[]){
     if(n<0)
         ErrorMessage("Error reading socket");
     printf("%s\n",buffer);
+	
+	
+	
     return 0;
 }
--- server.c
+++ server.c
@@ -5,18 +5,86 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+
+struct list {
+char *string;
+struct list *next;
+};
+
+typedef struct list LIST;
  
 void ErrorMessage(const char *msg){
     perror(msg);
     return;
 }
  
+
+int opentext(char *username, char *password){
+    FILE *ptr_file;
+	//int x;
+	char buf[1000];
+	char *token;
+	LIST *current, *head;
+	head = current = NULL;
+	ptr_file =fopen("Authentication.txt", "r"); //opening a file to check authentification users and passwords
+		if (!ptr_file){
+			return 1;
+		}
+	while (fgets(buf,1000, ptr_file)!=NULL){
+        		//puts into list
+			LIST *node = malloc(sizeof(LIST));
+			node->string = strdup(buf);
+			node->next = NULL;
+			
+			if(head ==NULL){
+				current = head = node;
+			} else{
+				current = current->next = node;
+			}
+	}
+	fclose(ptr_file);
+	
+	for(current=head; current; current=current->next){
+		printf("%s", current->string);
+		/* get the first token */
+	   token = strtok(current->string, " \t");
+	   printf( "%s\n", token);
+	   printf( "%s\n", username);
+	   /* walk through other tokens */
+	   
+		  //printf( " %s\n", token);
+		  if(*token==*username){
+			  token = strtok(NULL, "\t");
+			  printf( " it works2!\n");
+			  if(*token==*password){
+				  printf( "password accepted");
+				  return 2;
+			  }
+		  }
+		
+		  
+	   
+		
+	}
+	
+	printf( "You entered either an incorrect username or password - disconnecting");
+	/*current = head;
+	
+	printf("%s", current->string);*/
+	
+	
+    return 0;
+}
 int main(int argc,char *argv[]){
+	
+	
  
     int sock_fd=0,new_sock_fd=0,port=0,clilen=0,n=0;
     char buffer[256];
     struct sockaddr_in serv_addr,cli_addr;
- 
+	opentext("Username", "Password");
+	
     if(argc<2){
         fprintf(stderr,"Error port is missing\n");
         return 0;
@@ -46,14 +114,23 @@ int main(int argc,char *argv[]){
  
     bzero(buffer,256);
     n=read(new_sock_fd,buffer,255);
- 
+	
+	
+
+		
     if(n<0)
         ErrorMessage("Error reading socket");
     printf("Here is the message:%s",buffer);
  
-    n=write(new_sock_fd,"Got your message",16);
+    n=write(new_sock_fd,"Got your message\n",16);
+	
+	n=write(new_sock_fd,"whats your password: ",16);
  
     if(n<0)
         ErrorMessage("Error writing on socket");
+	//opentext();
     return 0;
 }
+
+
+
